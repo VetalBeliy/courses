42a6c8db1e4f31e2110d31b6255b0cddbd19d07c
VetalBeliy <vbelokurenko@gmail.com>
2015-12-25 21:40:10 +0200

    update teachers_model.php
    add function get teacher on id

--- Ignat/application/models/teachers_model.php
+++ Ignat/application/models/teachers_model.php
@@ -2,9 +2,19 @@
 
 class Teachers_model extends CI_Model {
 
-  function get_teachers()
-    {
-        $query = $this->db->get('teachers');
-        return $query->result_array();
-    }
+	function get_all_teachers() {
+		$this->db->select('*');
+		$this->db->from('teachers');
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+	function get_teachers($n) {
+		$this->db->select('*');
+		$this->db->from('teachers');
+		$this->db->where("teachers.id=$n");
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
 }
