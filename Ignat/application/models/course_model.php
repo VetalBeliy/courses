3a7b56b4353832b8633c58ecf9b7e172ab7aa7fb
Alona <alenija23@gmail.com>
2015-12-26 02:27:18 +0200

    Эх раз, ещё раз, ещё много-много раз)

--- Ignat/application/models/course_model.php
+++ Ignat/application/models/course_model.php
@@ -5,19 +5,12 @@ class Course_model extends CI_Model {
 
     function get_course_all($num, $offset)
     {
-       // $this->db->order_by('id','desc');
         $this->db->select('*');
         $this->db->from('Course');
         $this->db->join('teachers', 'teachers.id = Course.teacher_id');
-        //$query = $this->db->get();
+
         $query = $this->db->get('', $num, $offset);
         return $query->result_array();
-
-        // $this->db->select('*');
-        // $this->db->from('Course');
-        // $this->db->join('teachers', 'teachers.id = Course.teacher_id');
-        // $query = $this->db->get();
-        // return $query->result_array();
     }
 
 
