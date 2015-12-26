0b20ee29c3b40ea6fc5591ebd2086ceb08130374
VetalBeliy <vbelokurenko@gmail.com>
2015-12-26 12:27:26 +0200

    according to a General logic

--- Ignat/application/controllers/teachers.php
+++ Ignat/application/controllers/teachers.php
@@ -2,18 +2,19 @@
 
 class Teacher extends CI_Controller {
 
-    function teachers_all_view()
+    public function index($n = 0)
     {
-        $this->load->model('teachers_model');
-        $data['teachers'] = $this->teachers_model->get_all_teachers();
-        $this->load->view('teachers_view', $data);
-    }
-
-    function teachers_view($n)
-    {
+        if($n != 0) {
         $this->load->model('teachers_model');
         $data['teachers'] = $this->teachers_model->get_teachers($n);
         $this->load->view('teachers_view', $data);
+        }
+
+    else{
+        $this->load->model('teachers_model');
+        $data['teachers'] = $this->teachers_model->get_all_teachers();
+        $this->load->view('teachers_view', $data);
+        }
     }
 
 }
